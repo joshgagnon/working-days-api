5a4131bcf971f33ed5d1745c39a6cce35fef3d9a
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-06-21 18:18:07 +1200

    remove func from tables file

--- tables.sql
+++ tables.sql
@@ -4,13 +4,3 @@ CREATE INDEX ON holidays(day);
 CREATE INDEX idxgin ON holidays USING gin (summary_flags);
 
 
-
-CREATE OR REPLACE FUNCTION working_days(start date, count integer, flags text[])
-    RETURNS date
-    AS $$
-        SELECT day FROM holidays
-            WHERE day >= start AND NOT (summary_flags ?| $3)
-            ORDER BY day
-            OFFSET count
-            LIMIT 1
-    $$ LANGUAGE SQL;
