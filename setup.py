1b969036d281031e8d1c6aaa635de3787f2bca3f
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-06-22 10:47:03 +1200

    switched to requirements file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='working-days-api',
       version='0.0.1',
       description='',
-      url='http://github.com/joshgagnon/workig-days-api',
+      url='http://github.com/joshgagnon/working-days-api',
       author='Joshua Gagnon',
       author_email='josh.n.gagnon@gmail.com',
       license='MIT',
