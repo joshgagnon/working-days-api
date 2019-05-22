5adead9b9d95fb7c7df29de6280f2e9e367fe89d
Joshua Gagnon <josh.n.gagnon@gmail.com>
2019-05-22 11:01:52 +1000

    tweaked mode

--- server.py
+++ server.py
@@ -64,7 +64,7 @@ def calculate_period(cur, args):
     direction = args.get('direction', 'positive')
     if scheme in ['property', 'land_transfer', 'agreement_sale_purchase_real_estate']:
         flags.append(args['region'])
-    if args.get('calendar_days'):
+    if args.get('mode') == 'calendar_days':
         flags = []
     target = datetime.strptime(start_date, "%Y-%m-%d").date()
 
